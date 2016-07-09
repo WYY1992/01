e141c9022ce21f59ceae6272405a2affeb34878e
WYY <1024673005@qq.com>
2016-07-09 23:39:20 +0800

    fib

---
+++ fib.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+void main ()
+{
+    int n, i;
+    scanf("%d", &n);
+    int F[n-1];
+    F[0] = 0;
+    F[1] = 1;
+    int *p0, *p1;
+    p0 = &F[0];
+	p1 = &F[1];
+	
+	for (i = 2; i <= n-1; i++, p0++, p1++)
+	{
+		F[i] = *p1 + *p1;
+	}
+	printf("%d\n", F[n-1]);
+}
